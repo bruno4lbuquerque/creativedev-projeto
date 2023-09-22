4a87cb5de7170653df5b011c32fca5400de0a6c2
Bruno Albuquerque <=>
2023-09-21 20:41:59 -0400

    new page and effect scroll

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.headline', { duration: 1000 });
+
+sr.reveal('.client', { duration: 1000 });
+
+sr.reveal('.gallery-image', { duration: 1000 });
+
+sr.reveal('.iphone-x', { duration: 1000 });
+
+sr.reveal('.item', { duration: 1000 });
+
+sr.reveal('.feature', { duration: 1000 });
+
+sr.reveal('.btn', { duration: 1000 });
+
+sr.reveal('.principal', { duration: 1000 });
